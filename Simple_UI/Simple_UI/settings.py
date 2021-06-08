d809b32b48e80b47ae26bfc0572f828ff25b9dd1
mauroxcf <maurox03@gmail.com>
2021-06-08 00:43:35 -0500

    se elimino swagger por problemas de warnings, y se hicieron ajuste

--- Simple_UI/Simple_UI/settings.py
+++ Simple_UI/Simple_UI/settings.py
@@ -6,6 +6,8 @@ from datetime import timedelta
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+#fix warning
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 # Quick-start development settings - unsuitable for production
 
